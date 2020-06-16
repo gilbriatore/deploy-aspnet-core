c0f6bb1700a02e7e250c4a7cb611a2e2b4ecb3ee
gilbrilhador <geucimar@gmail.com>
2020-06-16 02:57:03 -0300

    Versão inicial.

---
+++ AppMonoWeb/AppMonoWeb/Data/PessoaContext.cs
@@ -0,0 +1,28 @@
+﻿using AppMonoWeb.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppMonoWeb.Data
+{
+    public class PessoaContext : DbContext
+    {
+        public DbSet<Pessoa> Pessoas { get; set; }
+
+        /**
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AppMonoWeb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+        */
+
+        public PessoaContext(DbContextOptions<PessoaContext> options)
+    : base(options)
+        {
+        }
+
+    }
+}
